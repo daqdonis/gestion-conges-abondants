c11d2dee75d07463c66b0cd8211aecc97ac7fc28
nada-bzt <nadabouzit13@gmail.com>
2025-05-07 19:06:20 +0200

    mn

--- src/main/java/com/groupe14ing2/gestioncongesabondants/controllers/DatabaseLink.java
+++ src/main/java/com/groupe14ing2/gestioncongesabondants/controllers/DatabaseLink.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
  public class DatabaseLink {
     protected static final String URL = "jdbc:mysql://localhost:3306/conges_abondant"; // Change to your MySQL URL
-    protected static final String USER = "z_nour"; // Change to your MySQL username
-    protected static final String PASSWORD = "NOUR@05houda"; // Change to your MySQL password
+    protected static final String USER = "nada"; // Change to your MySQL username
+    protected static final String PASSWORD = "Nada2005;"; // Change to your MySQL password
     protected final Connection connection;
 
     public DatabaseLink() throws SQLException {
