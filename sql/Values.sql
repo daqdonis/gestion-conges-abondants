dc1ca586db801fb4dcf7889724113171d76be565
Eggy <yassermhamedi333@gmail.com>
2025-05-16 20:48:02 +0100

    Change the database-schema.sql and Values.sql

--- sql/Values.sql
+++ sql/Values.sql
@@ -31,3 +31,72 @@ VALUES (1, "Mhamed", "Yasser", "admin_conge", "aa", "$2a$10$U3XviDJRJCa7/4oicje1
 /* here we are using the password "aa" for the admin .*/
 -- The password is hashed using bcrypt with a cost factor of 10.
 
+
+-------------------
+
+-- new Values
+-- Filiere (XXX-0)
+INSERT INTO Filiere VALUES
+('INF-1', 'Informatique'),
+('GES-1', 'Gestion'),
+('RES-1', 'Réseaux');
+
+-- Cycle (AAA)
+INSERT INTO Cycle VALUES
+('LIC', 'Licence Informatique', 'INF-1'),
+('LGE', 'Licence Gestion', 'GES-1'),
+('MAS', 'Master Informatique', 'INF-1');
+
+-- Semestre (CYCLE-NUM)
+INSERT INTO Semestre VALUES
+('LIC-1', 1, 'LIC'),
+('LIC-2', 2, 'LIC'),
+('LGE-1', 1, 'LGE'),
+('MAS-1', 1, 'MAS');
+
+-- Section (CYCLE-S-NUM)
+INSERT INTO Section VALUES
+('LIC-S1', 1, 'LIC'),
+('LIC-S2', 2, 'LIC'),
+('LGE-S1', 1, 'LGE');
+
+-- Groupe (SECTION-G-NUM)
+INSERT INTO Groupe VALUES
+('LIC-S1-G1', 1, 'LIC-S1', 'LIC-1'),
+('LIC-S1-G2', 2, 'LIC-S1', 'LIC-1'),
+('LGE-S1-G1', 1, 'LGE-S1', 'LGE-1');
+
+-- Etudiant (8-digit)
+INSERT INTO Etudiant VALUES
+('32011001', 'Dupont', 'Jean', '2000-05-15', 'LIC-S1-G1'),
+('32011002', 'Martin', 'Sophie', '2001-08-22', 'LIC-S1-G1'),
+('32011003', 'Bernard', 'Pierre', '2000-11-30', 'LIC-S1-G2');
+
+-- Admin (X-X-000)
+INSERT INTO Admin VALUES
+('TA001', 'a', 'a', 'admin_comptes', 'b', '$2a$10$nQvz9t0wrhgUH7wPSr7tI.C/ErCuZ4hRmstKwBXCJkH0vYh5Bgdly', 'T', 'A'),
+('RC001', 'homsin', 'za', 'admin_conge', 'c', '$2a$10$RGPyNRVeQxXv3IwCwdQt4eo/3O5vkC99XgMzMjuzrXdhRzIkuXtXK', 'R', 'C');
+
+-- Module (FILIERE-NIVEAU-SEMESTRE-MODULE)
+INSERT INTO Module VALUES
+('INF-1-L1-ALG', 'Algorithmique', 'LIC-1', 'INF-1', 'L1'),
+('INF-1-L1-PRG', 'Programmation', 'LIC-1', 'INF-1', 'L1'),
+('INF-1-L2-SYS', 'Systèmes', 'LIC-2', 'INF-1', 'L2'),
+('GES-1-L1-ECO', 'Economie', 'LGE-1', 'GES-1', 'L1');
+
+-- Note_module
+INSERT INTO Note_module VALUES
+('32011001', 'INF-1-L1-ALG', 15.5, 14.0, 12.0),
+('32011001', 'INF-1-L1-PRG', 16.0, 13.5, 14.0);
+
+-- Conge (TYPE-ANNEE-MATRICULE)
+INSERT INTO Conge VALUES
+('C-25-32011001', '32011001', '2025-01-10', 30, 'Accepté', 'Justificatif1');
+
+-- Abondant
+INSERT INTO Abondant VALUES
+('32011003', 'TA001', '2025-03-01');
+
+-- Action_admin
+INSERT INTO Action_admin VALUES
+('TA001', 'Validation congé', NOW(), 'C-25-32011001', NULL, NULL);
--- sql/database-schema.sql
+++ sql/database-schema.sql
@@ -138,3 +138,139 @@ CREATE TABLE IF NOT EXISTS Action_admin(
     FOREIGN KEY (id_reins) REFERENCES Dem_reins(id_demande),
     FOREIGN KEY (pk_abond) REFERENCES Abondant(id_etu)
 );
+
+-----
+-- new database creation script
+DROP DATABASE IF EXISTS conges_abondant;
+CREATE DATABASE conges_abondant;
+USE conges_abondant;
+
+-- Filiere (XXX-0)
+CREATE TABLE Filiere(
+    id_filiere VARCHAR(10) PRIMARY KEY,
+    design_filiere VARCHAR(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Cycle (AAA or AAA+number)
+CREATE TABLE Cycle(
+    id_cycle VARCHAR(4) PRIMARY KEY,
+    design_cycle VARCHAR(63) NOT NULL,
+    id_filiere VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_filiere) REFERENCES Filiere(id_filiere)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Semestre (CYCLE-NUM)
+CREATE TABLE Semestre(
+    id_semestre VARCHAR(10) PRIMARY KEY,
+    num_semestre INT NOT NULL,
+    id_cycle VARCHAR(4) NOT NULL,
+    FOREIGN KEY (id_cycle) REFERENCES Cycle(id_cycle)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Section (S-NUM)
+CREATE TABLE Section(
+    id_section VARCHAR(10) PRIMARY KEY,
+    num_section INT NOT NULL,
+    id_cycle VARCHAR(4) NOT NULL,
+    FOREIGN KEY (id_cycle) REFERENCES Cycle(id_cycle)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Groupe (G-NUM)
+CREATE TABLE Groupe(
+    id_groupe VARCHAR(10) PRIMARY KEY,
+    num_groupe INT NOT NULL,
+    id_section VARCHAR(10) NOT NULL,
+    id_semestre VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_section) REFERENCES Section(id_section),
+    FOREIGN KEY (id_semestre) REFERENCES Semestre(id_semestre)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Etudiant (8-digit)
+CREATE TABLE Etudiant(
+    id_etu VARCHAR(8) PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    date_naiss DATE NOT NULL,
+    id_groupe VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_groupe) REFERENCES Groupe(id_groupe)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Conge (X-AA-00000000)
+CREATE TABLE Conge(
+    id_demande VARCHAR(15) PRIMARY KEY,
+    id_etu VARCHAR(8) NOT NULL,
+    date_demande DATE NOT NULL,
+    duree INT,
+    etat ENUM('En attente','Refusé','Accepté') DEFAULT 'En attente',
+    justificatif MEDIUMBLOB NOT NULL,
+    FOREIGN KEY (id_etu) REFERENCES Etudiant(id_etu)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dem_reins (X-AA-00000000)
+CREATE TABLE Dem_reins(
+    id_demande VARCHAR(15) PRIMARY KEY,
+    id_etu VARCHAR(8) NOT NULL,
+    date_demande DATE NOT NULL,
+    justificatif MEDIUMBLOB NOT NULL,
+    etat ENUM('En attente','Refusé','Accepté') DEFAULT 'En attente',
+    FOREIGN KEY (id_etu) REFERENCES Etudiant(id_etu)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Module (XXX-0-X0-XXX)
+CREATE TABLE Module(
+    id_module VARCHAR(31) PRIMARY KEY,
+    nom_module VARCHAR(31) NOT NULL,
+    id_semestre VARCHAR(10) NOT NULL,
+    id_filiere VARCHAR(10) NOT NULL,
+    niveau_etude VARCHAR(3) NOT NULL,
+    FOREIGN KEY (id_semestre) REFERENCES Semestre(id_semestre),
+    FOREIGN KEY (id_filiere) REFERENCES Filiere(id_filiere)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Note_module
+CREATE TABLE Note_module(
+    id_etu VARCHAR(8) NOT NULL,
+    id_module VARCHAR(31) NOT NULL,
+    note_td FLOAT,
+    note_tp FLOAT,
+    note_exam FLOAT,
+    PRIMARY KEY (id_etu, id_module),
+    FOREIGN KEY (id_etu) REFERENCES Etudiant(id_etu),
+    FOREIGN KEY (id_module) REFERENCES Module(id_module)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Admin (X-X-000)
+CREATE TABLE Admin(
+    id_admin VARCHAR(10) PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    roles SET('admin_conge','admin_abondant','admin_comptes') NOT NULL,
+    email VARCHAR(63) NOT NULL,
+    mot_passe VARCHAR(63) NOT NULL,
+    type_compte CHAR(1) NOT NULL,
+    type_demande_traite CHAR(1)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Abondant
+CREATE TABLE Abondant(
+    id_etu VARCHAR(8) PRIMARY KEY,
+    id_admin VARCHAR(10) NOT NULL,
+    date_dec DATE NOT NULL,
+    FOREIGN KEY (id_etu) REFERENCES Etudiant(id_etu),
+    FOREIGN KEY (id_admin) REFERENCES Admin(id_admin)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Action_admin
+CREATE TABLE Action_admin(
+    id_action INT AUTO_INCREMENT PRIMARY KEY,
+    id_admin VARCHAR(10) NOT NULL,
+    action TINYTEXT NOT NULL,
+    temps_action TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_conge VARCHAR(15),
+    id_reins VARCHAR(15),
+    pk_abond VARCHAR(8),
+    FOREIGN KEY (id_admin) REFERENCES Admin(id_admin),
+    FOREIGN KEY (id_conge) REFERENCES Conge(id_demande),
+    FOREIGN KEY (id_reins) REFERENCES Dem_reins(id_demande),
+    FOREIGN KEY (pk_abond) REFERENCES Abondant(id_etu)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
