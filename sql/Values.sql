2492d5a3cfbab48f6b79876bea695af96086fe14
eggy <yassermhamedi333@gmail.com>
2025-05-18 20:51:47 +0100

    Update the Values again with 3 type of admin

--- sql/Values.sql
+++ sql/Values.sql
@@ -1,38 +1,3 @@
--- Insert Filiere
-INSERT INTO Filiere (design_filiere) VALUES ('Informatique'), ('Gestion'), ('Réseaux');
-
--- Insert Cycle
-INSERT INTO Cycle (design_cycle, id_filiere) VALUES 
-('Licence', 1), 
-('Master', 1), 
-('Licence', 2);
-
--- Insert Semestre
-INSERT INTO Semestre (num_semestre) VALUES (1), (2), (3), (4), (5), (6);
-
--- Insert Section
-INSERT INTO Section (num_section, id_cycle) VALUES (1, 1), (2, 1), (1, 2);
-
--- Insert Groupe
-INSERT INTO Groupe (num_groupe, id_section, id_semestre) VALUES 
-(1, 1, 1), 
-(2, 1, 1), 
-(1, 2, 3);
-
--- Insert Etudiant (no demand references needed now)
-INSERT INTO Etudiant (id_etu, nom, prenom, date_naiss, id_groupe) VALUES 
-(12345, 'Mhamedi', 'Yasser', '2005-11-23', 1),
-(12346, 'Smith', 'John', '2004-05-15', 2);
-
-INSERT INTO admin (id_admin, nom, prenom, roles, email, mot_passe)
-
-
-VALUES (1, "Mhamed", "Yasser", "admin_conge", "aa", "$2a$10$U3XviDJRJCa7/4oicje14.C6jpNxw99gf15qLDY2C2k/rFBRc0M76");
-/* here we are using the password "aa" for the admin .*/
--- The password is hashed using bcrypt with a cost factor of 10.
-
-
--------------------
 
 -- new Values
 -- Filiere (XXX-0)
@@ -74,8 +39,9 @@ INSERT INTO Etudiant VALUES
 
 -- Admin (X-X-000)
 INSERT INTO Admin VALUES
-('TA001', 'a', 'a', 'admin_comptes', 'b', '$2a$10$nQvz9t0wrhgUH7wPSr7tI.C/ErCuZ4hRmstKwBXCJkH0vYh5Bgdly', 'T', 'A'),
-('RC001', 'homsin', 'za', 'admin_conge', 'c', '$2a$10$RGPyNRVeQxXv3IwCwdQt4eo/3O5vkC99XgMzMjuzrXdhRzIkuXtXK', 'R', 'C');
+     ('TC001', 'Ahmed', 'Mohsin', 'admin_abondant', 'a', '$2a$10$coOsPNKaVhCYniC41qlw1uuuZThJHwSDWMxtzaZHLPRtHZRMFR.Bm'),
+     ('RC001', 'Mhamed', 'Yasser', 'admin_comptes', 'b', '$2a$10$nQvz9t0wrhgUH7wPSr7tI.C/ErCuZ4hRmstKwBXCJkH0vYh5Bgdly'),
+     ('TA001', 'za', 'za', 'admin_conge', 'c', '$2a$10$RGPyNRVeQxXv3IwCwdQt4eo/3O5vkC99XgMzMjuzrXdhRzIkuXtXK');
 
 -- Module (FILIERE-NIVEAU-SEMESTRE-MODULE)
 INSERT INTO Module VALUES
@@ -98,5 +64,5 @@ INSERT INTO Abondant VALUES
 ('32011003', 'TA001', '2025-03-01');
 
 -- Action_admin
-INSERT INTO Action_admin VALUES
-('TA001', 'Validation congé', NOW(), 'C-25-32011001', NULL, NULL);
+INSERT INTO Action_admin (id_admin, action, temps_action, id_conge, id_reins, pk_abond)
+VALUES ('TA001', 'Validation congé', NOW(), 'C-25-32011001', NULL, NULL);
