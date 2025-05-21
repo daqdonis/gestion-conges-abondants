1dbbc4a81bf834552c9ed043e5e04707aac59d96
Eggy <yassermhamedi333@gmail.com>
2025-05-21 02:04:24 +0100

    Fixed the insertion in Values.sql

--- sql/Values.sql
+++ sql/Values.sql
@@ -1,5 +1,3 @@
-
--- new Values
 -- Filiere (XXX-0)
 INSERT INTO Filiere VALUES
 ('INF-1', 'Informatique'),
@@ -26,42 +24,31 @@ INSERT INTO Section VALUES
 ('LGE-S1', 1, 'LGE');
 
 -- Groupe (SECTION-G-NUM)
+
 INSERT INTO Groupe VALUES
-('LIC-S1-G1', 1, 'LIC-S1', 'LIC-1'),
-('LIC-S1-G2', 2, 'LIC-S1', 'LIC-1'),
-('LGE-S1-G1', 1, 'LGE-S1', 'LGE-1');
+('LIC-S1-G1', 1, 'LIC-S1', 'LIC-1', 'LIC-2'),
+('LIC-S1-G2', 2, 'LIC-S1', 'LIC-1', 'LIC-2'),
+('LGE-S1-G1', 1, 'LGE-S1', 'LGE-1', 'LGE-1');
 
 -- Etudiant (8-digit)
 INSERT INTO Etudiant VALUES
-('32011001', 'Dupont', 'Jean', '2000-05-15', 'LIC-S1-G1'),
-('32011002', 'Martin', 'Sophie', '2001-08-22', 'LIC-S1-G1'),
-('32011003', 'Bernard', 'Pierre', '2000-11-30', 'LIC-S1-G2');
+(32011001, 'Dupont', 'Jean', '2000-05-15', 'LIC-S1-G1'),
+(32011002, 'Martin', 'Sophie', '2001-08-22', 'LIC-S1-G1'),
+(32011003, 'Bernard', 'Pierre', '2000-11-30', 'LIC-S1-G2');
 
 -- Admin (X-X-000)
 INSERT INTO Admin VALUES
-     ('TC001', 'Ahmed', 'Mohsin', 'admin_abondant', 'a', '$2a$10$coOsPNKaVhCYniC41qlw1uuuZThJHwSDWMxtzaZHLPRtHZRMFR.Bm'),
-     ('RC001', 'Mhamed', 'Yasser', 'admin_comptes', 'b', '$2a$10$nQvz9t0wrhgUH7wPSr7tI.C/ErCuZ4hRmstKwBXCJkH0vYh5Bgdly'),
-     ('TA001', 'za', 'za', 'admin_conge', 'c', '$2a$10$RGPyNRVeQxXv3IwCwdQt4eo/3O5vkC99XgMzMjuzrXdhRzIkuXtXK');
-
--- Module (FILIERE-NIVEAU-SEMESTRE-MODULE)
-INSERT INTO Module VALUES
-('INF-1-L1-ALG', 'Algorithmique', 'LIC-1', 'INF-1', 'L1'),
-('INF-1-L1-PRG', 'Programmation', 'LIC-1', 'INF-1', 'L1'),
-('INF-1-L2-SYS', 'Systèmes', 'LIC-2', 'INF-1', 'L2'),
-('GES-1-L1-ECO', 'Economie', 'LGE-1', 'GES-1', 'L1');
-
--- Note_module
-INSERT INTO Note_module VALUES
-('32011001', 'INF-1-L1-ALG', 15.5, 14.0, 12.0),
-('32011001', 'INF-1-L1-PRG', 16.0, 13.5, 14.0);
+('TC001', 'Ahmed', 'Mohsin', 'admin_abondant', 'a', '$2a$10$coOsPNKaVhCYniC41qlw1uuuZThJHwSDWMxtzaZHLPRtHZRMFR.Bm'),
+('RC001', 'Mhamed', 'Yasser', 'admin_comptes', 'b', '$2a$10$nQvz9t0wrhgUH7wPSr7tI.C/ErCuZ4hRmstKwBXCJkH0vYh5Bgdly'),
+('TA001', 'za', 'za', 'admin_conge', 'c', '$2a$10$RGPyNRVeQxXv3IwCwdQt4eo/3O5vkC99XgMzMjuzrXdhRzIkuXtXK');
 
 -- Conge (TYPE-ANNEE-MATRICULE)
 INSERT INTO Conge VALUES
-('C-25-32011001', '32011001', '2025-01-10', 30, 'Accepté', 'Justificatif1');
+('C-25-32011001', 32011001, '2025-01-10', 30, 'Accepté', 'Justificatif1');
 
 -- Abondant
 INSERT INTO Abondant VALUES
-('32011003', 'TA001', '2025-03-01');
+(32011003, 'TA001', '2025-03-01');
 
 -- Action_admin
 INSERT INTO Action_admin (id_admin, action, temps_action, id_conge, id_reins, pk_abond)
